366d506180875856f5c5186565486629c11db1f5
Ratan Kumar <krratan80@gmail.com>
2025-08-15 20:06:56 +0530

    Revert "Update in useAuthStore and axios instances"
    This reverts commit e399d56164aaeee1e6eabdda38b6369eb9b834c5.

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL =
-  import.meta.env.MODE === "production"
-    ? "https://mitralink-messanger-app.onrender.com/api"
-    : "http://localhost:8000/api";
 export const axiosInstance = axios.create({
-    baseURL: BASE_URL,
+    baseURL: "http://localhost:8000/api",
     withCredentials: true,
 })
